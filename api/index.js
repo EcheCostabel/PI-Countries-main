cdace102e56ecb0c676009a2d2407d542101edb8
EcheCostabel <exequielcostabel@gmail.com>
2022-10-05 17:57:28 -0300

    bugs

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const { PORT } = process.env;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
